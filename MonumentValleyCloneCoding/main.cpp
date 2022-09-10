bbbe5aceda6462046cba1f164e620f22353a18bf
DESKTOP-3V8EN6N\secul <secul5972@naver.com>
2022-09-10 20:08:06 +0900

    main code cleanup

--- MonumentValleyCloneCoding/main.cpp
+++ MonumentValleyCloneCoding/main.cpp
@@ -4,15 +4,10 @@
 #include "headerFile/shader.h"
 #include "headerFile/camera.h"
 #include "headerFile/model.h"
-//shape
 #include "headerFile/Shape.h"
 #include "headerFile/Level.h"
-//ClickArea
 #include "headerFile/ClickArea.h"
-
 #include "headerFile/AdditionalMathFunction.h"
-//#include <stb/stb_image.h>
-//#include <glad/glad.h>
 
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
@@ -22,37 +17,31 @@
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void mouse_callback(GLFWwindow* window, double xpos, double ypos);
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow* window);
-
-void MakeViewportMatrixWithoutDepth();
+void MakeViewportMatrix();
 
 // settings
 const unsigned int SCR_WIDTH = 1200;
 const unsigned int SCR_HEIGHT = 1200;
 
-//window
+// window
 GLFWwindow* window;
 
 // camera
 Camera camera(glm::vec3(5.0f, 5.0f, 5.0f), glm::vec3(0.0f, 1.0f, 0.0f), glm::vec3(-1.0f, -1.0f, -1.0f), -90.0f, 0.0f);
-float lastX = SCR_WIDTH / 2.0f;
-float lastY = SCR_HEIGHT / 2.0f;
-bool firstMouse = true;
 
 // timing
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
-glm::mat4 projection, view, worldModel;
-glm::mat4 viewport;
+// matrix
+glm::mat4 viewport, projection, view, worldModel;
+
+// light
 glm::vec3 lightPos, lightColor;
 
 bool left_mouse_button_down = false;
-///////////////////////////////////////////////////////////////
-Shader* aa;
-///////////////////////////////////////////////////////////////
+
 int main()
 {
 	// glfw: initialize and configure
@@ -75,11 +64,9 @@ int main()
 	}
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-	glfwSetCursorPosCallback(window, mouse_callback);
-	glfwSetScrollCallback(window, scroll_callback);
 
 	// tell GLFW to capture our mouse
-	//glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	// glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
 	// glad: load all OpenGL function pointers
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -91,25 +78,14 @@ int main()
 	// tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
 	stbi_set_flip_vertically_on_load(true);
 
-	// configure global opengl state
-	glEnable(GL_DEPTH_TEST);
-
 	// build and compile shaders
-	// -------------------------
 	Shader defaultShader("shader/default.vert", "shader/default.frag");
-	aa = new Shader("shader/test.vert", "shader/test.frag");
-	// load models
-	// -----------
-	//Model ourModel("objects/backpack/backpack.obj");
 
 	// viewport matrix
-	// ---------------
-	MakeViewportMatrixWithoutDepth();
+	MakeViewportMatrix();
 
 	// prepare_shapes
-	// ---------------
 	prepare_axes();
-	//MakeCircleVertex();
 	Cube a;
 	Cuboid b;
 	Goal c;
@@ -122,41 +98,41 @@ int main()
 	Corn m;
 	Character n;
 	Sphere o;
-	//light
-	// ----
+
+	// light setting
 	lightPos = glm::vec3(0.0f, 10.0f, 0.0f);
 	lightColor = glm::vec3(1.0f, 1.0f, 1.0f);
 	defaultShader.use();
 	defaultShader.setVec3("lightPos", lightPos);
 	defaultShader.setVec3("lightColor", lightColor);
 	defaultShader.unuse();
-	// render loop
-	// -----------
+
+	// configure global opengl state
+	glEnable(GL_DEPTH_TEST);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		// per-frame time logic
-		// --------------------
 		float currentFrame = static_cast<float>(glfwGetTime());
 		deltaTime = currentFrame - lastFrame;
 		lastFrame = currentFrame;
 
-		// input
-		// -----
 		processInput(window);
 
 		// render
-		// ------
 		glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// view/projection transformations
-		//projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+		// projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 		projection = glm::ortho(-2.0f,2.0f, -2.0f, 2.0f, 0.0f, 100.0f);
 		view = camera.GetViewMatrix();
 
-		//draw_shapes
+		// base translate
 		worldModel = glm::mat4(1.0f);
 		worldModel = glm::translate(worldModel, glm::vec3(0.0f, -0.4f, 1.2f));
+
+		// draw_shapes
 		draw_axes(defaultShader);
 		//a.draw(defaultShader, worldModel);
 		//b.draw(defaultShader, worldModel);
@@ -170,88 +146,36 @@ int main()
 		//m.draw(defaultShader, worldModel);
 		//n.draw(defaultShader, worldModel);
 		//o.draw(defaultShader, worldModel);
+		
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-		// -------------------------------------------------------------------------------
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
 	// glfw: terminate, clearing all previously allocated GLFW resources.
-	// ------------------------------------------------------------------
 	glfwTerminate();
 	return 0;
 }
 
-// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-// ---------------------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
 void processInput(GLFWwindow* window)
 {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
-
-	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-		camera.ProcessKeyboard(FORWARD, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		camera.ProcessKeyboard(BACKWARD, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		camera.ProcessKeyboard(LEFT, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		camera.ProcessKeyboard(RIGHT, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
-		camera.ProcessKeyboard(UP, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
-		camera.ProcessKeyboard(DOWN, deltaTime);
 }
 
-// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-// ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void MakeViewportMatrix()
 {
-	// make sure the viewport matches the new window dimensions; note that width and 
-	// height will be significantly larger than specified on retina displays.
-	glViewport(0, 0, width, height);
-}
+	viewport = glm::mat4(1.0f);
 
-// glfw: whenever the mouse moves, this callback is called
-// -------------------------------------------------------
-void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
-{
-	float xpos = static_cast<float>(xposIn);
-	float ypos = static_cast<float>(yposIn);
-
-	if (firstMouse)
-	{
-		lastX = xpos;
-		lastY = ypos;
-		firstMouse = false;
-	}
-
-	float xoffset = xpos - lastX;
-	float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
-
-	lastX = xpos;
-	lastY = ypos;
-
-	camera.ProcessMouseMovement(xoffset, yoffset);
-}
-
-// glfw: whenever the mouse scroll wheel scrolls, this callback is called
-// ----------------------------------------------------------------------
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-{
-	camera.ProcessMouseScroll(static_cast<float>(yoffset));
-}
-
-void MakeViewportMatrixWithoutDepth()
-{
-	glm::mat4 viewport_matrix(1.0f);
-
-	viewport_matrix[0][0] = SCR_WIDTH / 2;
-	viewport_matrix[3][0] = SCR_WIDTH / 2;
-	viewport_matrix[1][1] = SCR_HEIGHT / 2;
-	viewport_matrix[3][1] = SCR_HEIGHT / 2;
-	viewport_matrix[2][2] = 0.5f;
-	viewport_matrix[3][2] = 0.5f;
-
-	viewport = viewport_matrix;
+	viewport[0][0] = SCR_WIDTH / 2;
+	viewport[3][0] = SCR_WIDTH / 2;
+	viewport[1][1] = SCR_HEIGHT / 2;
+	viewport[3][1] = SCR_HEIGHT / 2;
+	viewport[2][2] = 0.5f;
+	viewport[3][2] = 0.5f;
 }
